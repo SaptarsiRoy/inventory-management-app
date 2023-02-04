9efaa25788d68907a7987e60eb73f24ae9344998
SaptarsiRoy <saptarsiroy12@gmail.com>
2023-02-04 22:54:24 +0530

    fixed check duplicate product not working

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -139,7 +139,8 @@ const checkBody = (body) => {
 // function to check for duplicate product
 const checkDuplicateProduct = async (product) => {
     // check if product already exists
-    const dup_product = await Product.findOne({ product });
+    const dup_product = await Product.findOne({ name: product.name, price: product.price, stock: product.stock });
+    console.log(dup_product);
     // return true if product does not exists
     return dup_product ? false : true;
 };
